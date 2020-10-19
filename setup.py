fd44753865fad9339a7507d807e4083937c2a195
HerryZeng <herry.zeng@gmail.com>
2020-10-19 11:42:50 +0800

    增加blog,config,comment应用

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='typeidea',
+    version='0.0.1',
+    packages=['blog', 'blog.migrations', 'typeidea', 'typeidea.settings'],
+    url='',
+    license='',
+    author='zengdongliang',
+    author_email='herry.zeng@gmail.com',
+    description=''
+)
--- typeidea/settings/base.py
+++ typeidea/settings/base.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'config.apps.ConfigConfig',
+    'comment.apps.CommentConfig',
 ]
 
 MIDDLEWARE = [
